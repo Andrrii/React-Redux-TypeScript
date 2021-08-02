147fd87ab42913977793ca7da83696072abbafb2
Andrii <butsaandrii@gmail.com>
2021-08-02 17:44:39 +0300

    Getting and work with Getdata(user)

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { UserActions } from '../hooks/useActions';
 import { useTypedSelector } from '../hooks/useTypedSelector';
 
 const UserList:React.FC = () => {
 
-
     const state = useTypedSelector(state => state.user) // Берем дані із стану
-    console.log(state);
+
+    const {fetchUsers} = UserActions()
+
+    useEffect(() => {
+        fetchUsers()
+    },[])
+
+    if(state.loading) {
+        return <h1>Loading...</h1>
+    }
+
+    if(state.error){
+        return <h1>{state.error}</h1>
+    }
     
     return (
         <div>
-            
+            {state.users.map(item => (
+                <div key={item.id}>{item.name}</div>
+            ))}
         </div>
     );
 };
